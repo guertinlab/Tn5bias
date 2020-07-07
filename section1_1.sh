023c3ee1c2e29151ca9bc5eeb531ad43ceb5a4cc
guertinlab <guertin@virginia.edu>
2020-07-07 10:33:22 -0400

    Create section1_1.sh

---
+++ section1_1.sh
@@ -0,0 +1,21 @@
+
+cd ~/Desktop/atac_test
+
+fasterq-dump SRR5123141
+fasterq-dump SRR5123142
+
+gzip *fastq
+
+mv SRR5123141_1.fastq.gz C1_gDNA_rep1_PE1.fastq.gz 
+mv SRR5123141_2.fastq.gz C1_gDNA_rep1_PE2.fastq.gz
+mv SRR5123142_1.fastq.gz C1_gDNA_rep2_PE1.fastq.gz
+mv SRR5123142_2.fastq.gz C1_gDNA_rep2_PE2.fastq.gz
+
+#you also need to download the human genome:
+#one can use the following command on a Linux:
+wget http://hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz
+wget https://hgdownload.cse.ucsc.edu/goldenpath/hg38/chromosomes/chrM.fa.gz
+
+#unzip 
+gunzip hg38.fa.gz
+gunzip chrM.fa.gz
