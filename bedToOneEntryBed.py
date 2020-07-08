944649c1c7c81c6087f4efd98f07c5949e9712ab
guertinlab <guertin@virginia.edu>
2020-07-08 16:33:20 -0400

    Add files via upload

--- bedToOneEntryBed.py
+++ bedToOneEntryBed.py
@@ -9,7 +9,7 @@ import glob
 
 def function(fqFileName):
     infile=open(fqFileName, 'r')
-    outfile=open(string.split(fqFileName,'.bed')[0]+'.oneentry.bed', 'w')
+    outfile=open(str.split(fqFileName,'.bed')[0]+'.oneentry.bed', 'w')
     while 1:
         line=infile.readline()
         if not line: break
@@ -22,18 +22,18 @@ def function(fqFileName):
 def main(argv):
     try:
         opts, args = getopt.getopt(argv, "i:h", ["infile=","help"])
-    except getopt.GetoptError, err:
-        print str(err)
+    except getopt.GetoptError as err:
+        print(str(err))
         sys.exit(2)
     infile = False
     for opt, arg in opts:
         if opt in ('-i', '--infile'):
             infile = arg
         elif opt in ('-h', '--help'):
-            print '\n./bedToOneEntryBed.py -i test_PE1_plus_not_scaled.bed'
+            print('\n./bedToOneEntryBed.py -i test_PE1_plus_not_scaled.bed')
             sys.exit()
     if infile:
-        print infile
+        print(infile)
         function(infile)
 if __name__ == "__main__":
     main(sys.argv[1:])
