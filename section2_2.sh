20a4243eeeedac0e2aec66a2633a28e8ee4aa320
guertinlab <guertin@virginia.edu>
2020-07-09 22:25:22 -0400

    Update section2_2.sh

--- section2_2.sh
+++ section2_2.sh
@@ -1,52 +1,45 @@
 
-#if the next section works out, then it will eventually be incorporated into the loop above.
 
-name=CEM-C7_untreated_rep2
-#separate PE1 and PE2 reads
-samtools view -b -f 0x0040 ${name}.bam > ${name}_PE1.bam
-samtools view -b -f 0x0080 ${name}.bam > ${name}_PE2.bam
-
-
-#separate plus and minus strand aligning reads:
-samtools view -bh -F 20 ${name}_PE1.bam > ${name}_PE1_plus.bam
-samtools view -bh -f 0x10 ${name}_PE1.bam > ${name}_PE1_minus.bam
-
-samtools view -bh -F 20 ${name}_PE2.bam > ${name}_PE2_plus.bam
-samtools view -bh -f 0x10 ${name}_PE2.bam > ${name}_PE2_minus.bam
-
-
-#run seqOutBias to get the bed
-seqOutBias hg38.fa ${name}_PE1_plus.bam --no-scale --bw=${name}_PE1_plus.bigWig --shift-counts --read-size=30
-seqOutBias hg38.fa ${name}_PE1_minus.bam --no-scale --bw=${name}_PE1_minus.bigWig --shift-counts --read-size=30
-seqOutBias hg38.fa ${name}_PE2_plus.bam --no-scale --bw=${name}_PE2_plus.bigWig --shift-counts --read-size=30
-seqOutBias hg38.fa ${name}_PE2_minus.bam --no-scale --bw=${name}_PE2_minus.bigWig --shift-counts --read-size=30
-
-# a python script to process the bed into a single entry per instance
-
+#comments are for Rivanna (in progress)
 wget https://raw.githubusercontent.com/guertinlab/Tn5bias/master/bedToOneEntryBed.py
-
-python bedToOneEntryBed.py CEM-C7_untreated_rep2_PE1_plus_not_scaled.bed
-python bedToOneEntryBed.py CEM-C7_untreated_rep2_PE1_minus_not_scaled.bed
-python bedToOneEntryBed.py CEM-C7_untreated_rep2_PE2_plus_not_scaled.bed
-python bedToOneEntryBed.py CEM-C7_untreated_rep2_PE2_minus_not_scaled.bed
-
+#module load gcc/7.1.0
+#module load seqoutbias/1.2.0
+#module load mvapich2/2.3.1
+#module load openmpi/3.1.4
+#module load gcc/system
+#module load intel/18.0  
+#module load bedtools/2.26.0
+#module load python/3.7.7
+#ijob -A guertinlab -c 1 -p standard -t 10:00:00
+for i in C1_gDNA_rep2.bam
+do
+    name=$(echo $i | awk -F".bam" '{print $1}')
+    echo $name
+#separate PE1 and PE2 reads
+    samtools view -b -f 0x0040 ${name}.bam > ${name}_PE1.bam
+    samtools view -b -f 0x0080 ${name}.bam > ${name}_PE2.bam
+#separate plus and minus strand aligning reads:
+    samtools view -bh -F 20 ${name}_PE1.bam > ${name}_PE1_plus.bam
+    samtools view -bh -f 0x10 ${name}_PE1.bam > ${name}_PE1_minus.bam
+    samtools view -bh -F 20 ${name}_PE2.bam > ${name}_PE2_plus.bam
+    samtools view -bh -f 0x10 ${name}_PE2.bam > ${name}_PE2_minus.bam
+#run seqOutBias to get the bed
+    seqOutBias hg38.fa ${name}_PE1_plus.bam --no-scale --out=no_scale.tbl --shift-counts --bw=${name}_PE1_plus.bigWig --read-size=30
+    seqOutBias hg38.fa ${name}_PE1_minus.bam --no-scale  --out=no_scale.tbl --shift-counts --bw=${name}_PE1_minus.bigWig --read-size=30
+    seqOutBias hg38.fa ${name}_PE2_plus.bam --no-scale  --out=no_scale.tbl --shift-counts --bw=${name}_PE2_plus.bigWig --read-size=30
+    seqOutBias hg38.fa ${name}_PE2_minus.bam --no-scale --out=no_scale.tbl --shift-counts --bw=${name}_PE2_minus.bigWig --read-size=30
+    python bedToOneEntryBed.py ${name}_PE1_plus_not_scaled.bed
+    python bedToOneEntryBed.py ${name}_PE1_minus_not_scaled.bed
+    python bedToOneEntryBed.py ${name}_PE2_plus_not_scaled.bed
+    python bedToOneEntryBed.py ${name}_PE2_minus_not_scaled.bed
 #these bed files can be used to get the sequence flanking ALL Tn5 insertion sites, 
 #so we can see the precise nature of the sequence bias for each PE/strand combination
-
-
-tn5File=CEM-C7_untreated_rep2_PE1_plus_not_scaled.bed
-awk '{$2 = $2 - 11; print}' $tn5File | awk '{OFS="\t";} {$3 = $2 + 20; print}' | grep -v - | \
-    fastaFromBed -fi hg38.fa -s -bed stdin -fo CEM-C7_untreated_rep2_PE1_plus_not_scaled.fasta
-
-tn5File=CEM-C7_untreated_rep2_PE1_minus_not_scaled.bed
-awk '{$2 = $2 - 11; print}' $tn5File | awk '{OFS="\t";} {$3 = $2 + 20; print}' |  grep -v - | \
-    fastaFromBed -fi hg38.fa -s -bed stdin -fo CEM-C7_untreated_rep2_PE1_minus_not_scaled.fasta
-
-
-tn5File=CEM-C7_untreated_rep2_PE2_plus_not_scaled.bed
-awk '{$2 = $2 - 11; print}' $tn5File | awk '{OFS="\t";} {$3 = $2 + 20; print}' | grep -v - | \
-    fastaFromBed -fi hg38.fa -s -bed stdin -fo CEM-C7_untreated_rep2_PE2_plus_not_scaled.fasta
-
-tn5File=CEM-C7_untreated_rep2_PE2_minus_not_scaled.bed
-awk '{$2 = $2 - 11; print}' $tn5File | awk '{OFS="\t";} {$3 = $2 + 20; print}' |  grep -v - | \
-    fastaFromBed -fi hg38.fa -s -bed stdin -fo CEM-C7_untreated_rep2_PE2_minus_not_scaled.fasta
+    awk '{$2 = $2 - 9; print}' ${name}_PE1_plus_not_scaled.bed | awk '{OFS="\t";} {$3 = $2 + 20; print}' | grep -v - | \
+       fastaFromBed -fi hg38.fa -s -bed stdin -fo ${name}_PE1_plus_not_scaled.fasta
+    awk '{$2 = $2 - 9; print}' ${name}_PE1_minus_not_scaled.bed | awk '{OFS="\t";} {$3 = $2 + 20; print}' |  grep -v - | \
+       fastaFromBed -fi hg38.fa -s -bed stdin -fo ${name}_PE1_minus_not_scaled.fasta
+    awk '{$2 = $2 - 9; print}' ${name}_PE2_plus_not_scaled.bed | awk '{OFS="\t";} {$3 = $2 + 20; print}' | grep -v - | \
+       fastaFromBed -fi hg38.fa -s -bed stdin -fo ${name}_PE2_plus_not_scaled.fasta
+    awk '{$2 = $2 - 9; print}' ${name}_PE2_minus_not_scaled.bed | awk '{OFS="\t";} {$3 = $2 + 20; print}' |  grep -v - | \
+       fastaFromBed -fi hg38.fa -s -bed stdin -fo ${name}_PE2_minus_not_scaled.fasta
+done
