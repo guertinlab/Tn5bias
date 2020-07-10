76ce31cb7a48e6b52bc12367a7538ebcc79a3cf3
JWolpe <66966976+JWolpe@users.noreply.github.com>
2020-07-10 14:07:07 -0400

    Update section2_2.sh

--- section2_2.sh
+++ section2_2.sh
@@ -1,93 +1,61 @@
-#comments are for Rivanna (in progress)
-wget https://raw.githubusercontent.com/guertinlab/Tn5bias/master/bedToOneEntryBed.py
-#module load gcc/7.1.0
-#module load seqoutbias/1.2.0
-#module load bedtools/2.26.0
-#module load intel/20.0  
-#module load intelmpi/20.0
-#module load python/3.7.7
-#ijob -A guertinlab -c 1 -p standard -t 10:00:00
-
-for i in C1_*_PE1_plus.bam
+for ba in C1*
 do
-    name=$(echo $i | awk -F"_PE1_plus.bam" '{print $1}')
-    echo $name
-#separate PE1 and PE2 reads
-    samtools view -b -f 0x0040 ${name}.bam > ${name}_PE1.bam
-    samtools view -b -f 0x0080 ${name}.bam > ${name}_PE2.bam
-#separate plus and minus strand aligning reads:
-    samtools view -bh -F 20 ${name}_PE1.bam > ${name}_PE1_plus.bam
-    samtools view -bh -f 0x10 ${name}_PE1.bam > ${name}_PE1_minus.bam
-    samtools view -bh -F 20 ${name}_PE2.bam > ${name}_PE2_plus.bam
-    samtools view -bh -f 0x10 ${name}_PE2.bam > ${name}_PE2_minus.bam
-#run seqOutBias to get the bed
-    seqOutBias hg38.fa ${name}_PE1_plus.bam --no-scale --out=no_scale.tbl --shift-counts \
-                                 --bed=${name}_PE1_plus_shift_counts.bed \
-                                 --bw=${name}_PE1_plus_shift_counts.bigWig --read-size=30
-    seqOutBias hg38.fa ${name}_PE1_minus.bam --no-scale --out=no_scale.tbl --shift-counts \
-                                 --bed=${name}_PE1_minus_shift_counts.bed \
-                                 --bw=${name}_PE1_minus_shift_counts.bigWig --read-size=30
-    seqOutBias hg38.fa ${name}_PE2_plus.bam --no-scale --out=no_scale.tbl --shift-counts \
-                                 --bed=${name}_PE2_plus_shift_counts.bed \
-                                 --bw=${name}_PE2_plus_shift_counts.bigWig --read-size=30
-    seqOutBias hg38.fa ${name}_PE2_minus.bam --no-scale --out=no_scale.tbl --shift-counts \
-                                 --bed=${name}_PE2_minus_shift_counts.bed \
-                                 --bw=${name}_PE2_minus_shift_counts.bigWig --read-size=30
-    python bedToOneEntryBed.py -i ${name}_PE1_plus_shift_counts.bed
-    python bedToOneEntryBed.py -i ${name}_PE1_minus_shift_counts.bed
-    python bedToOneEntryBed.py -i ${name}_PE2_plus_shift_counts.bed
-    python bedToOneEntryBed.py -i ${name}_PE2_minus_shift_counts.bed
-#these bed files can be used to get the sequence flanking ALL Tn5 insertion sites, 
-#so we can see the precise nature of the sequence bias for each PE/strand combination
-    awk '{$2 = $2 - 9; print}' ${name}_PE1_plus_shift_counts.oneentry.bed | \
-       awk '{OFS="\t";} {$3 = $2 + 20; print}' | grep -v - | \
-       fastaFromBed -fi hg38.fa -s -bed stdin -fo ${name}_PE1_plus_shift_counts.fasta
-    awk '{$2 = $2 - 9; print}' ${name}_PE1_minus_shift_counts.oneentry.bed | \
-       awk '{OFS="\t";} {$3 = $2 + 20; print}' |  grep -v - | \
-       fastaFromBed -fi hg38.fa -s -bed stdin -fo ${name}_PE1_minus_not_scaled_shift_counts.fasta
-    awk '{$2 = $2 - 9; print}' ${name}_PE2_plus_shift_counts.oneentry.bed | \
-       awk '{OFS="\t";} {$3 = $2 + 20; print}' | grep -v - | \
-       fastaFromBed -fi hg38.fa -s -bed stdin -fo ${name}_PE2_plus_shift_counts.fasta
-    awk '{$2 = $2 - 9; print}' ${name}_PE2_minus_shift_counts.oneentry.bed | \
-       awk '{OFS="\t";} {$3 = $2 + 20; print}' |  grep -v - | \
-       fastaFromBed -fi hg38.fa -s -bed stdin -fo ${name}_PE2_minus_shift_counts.fasta
+	name=$(echo $ba)
+	#Set window size for FASTA	
+	winsize=20
+	minusshift=$(expr $winsize / 2 - 1)	
+	echo $name
+	echo ${name:0:-4}
+	echo Window size is $winsize\bp
+	echo Window begins $minusshift\bp before insertion event
+	
+	#separate PE1 and PE2 reads
+	samtools view -b -f 0x0040 ${name:0:-4}.bam > ${name:0:-4}_PE1.bam
+	samtools view -b -f 0x0080 ${name:0:-4}.bam > ${name:0:-4}_PE2.bam
+
+
+	#separate plus and minus strand aligning reads:
+	samtools view -bh -F 20 ${name:0:-4}_PE1.bam > ${name:0:-4}_PE1_plus.bam
+	samtools view -bh -f 0x10 ${name:0:-4}_PE1.bam > ${name:0:-4}_PE1_minus.bam
+
+	samtools view -bh -F 20 ${name:0:-4}_PE2.bam > ${name:0:-4}_PE2_plus.bam
+	samtools view -bh -f 0x10 ${name:0:-4}_PE2.bam > ${name:0:-4}_PE2_minus.bam
+
+
+	#run seqOutBias to get the bed
+	seqOutBias hg38.fa ${name:0:-4}_PE1_plus.bam --no-scale --bw=${name:0:-4}_PE1_plus.bigWig --shift-counts --read-size=30
+	seqOutBias hg38.fa ${name:0:-4}_PE1_minus.bam --no-scale --bw=${name:0:-4}_PE1_minus.bigWig --shift-counts --read-size=30
+	seqOutBias hg38.fa ${name:0:-4}_PE2_plus.bam --no-scale --bw=${name:0:-4}_PE2_plus.bigWig --shift-counts --read-size=30
+	seqOutBias hg38.fa ${name:0:-4}_PE2_minus.bam --no-scale --bw=${name:0:-4}_PE2_minus.bigWig --shift-counts --read-size=30
+
+
+
+	python bedToOneEntryBed.py -i ${name:0:-4}_PE1_plus_not_scaled.bed
+	python bedToOneEntryBed.py -i ${name:0:-4}_PE1_minus_not_scaled.bed
+	python bedToOneEntryBed.py -i ${name:0:-4}_PE2_plus_not_scaled.bed
+	python bedToOneEntryBed.py -i ${name:0:-4}_PE2_minus_not_scaled.bed
+
+	#these bed files can be used to get the sequence flanking ALL Tn5 insertion sites, 
+	#so we can see the precise nature of the sequence bias for each PE/strand combination
+
+
+	tn5File=${name:0:-4}_PE1_plus_not_scaled.oneentry.bed
+	awk -v m=$minusshift '{$2 = $2 - m; print}' $tn5File | awk -v w=$winsize '{OFS="\t";} {$3 = $2 + w; print}' | grep -v - | \
+    		fastaFromBed -fi hg38.fa -s -bed stdin -fo ${name:0:-4}_PE1_plus_not_scaled.fasta
+
+	tn5File=${name:0:-4}_PE1_minus_not_scaled.oneentry.bed
+	awk -v m=$minusshift '{$2 = $2 - m; print}' $tn5File | awk -v w=$winsize '{OFS="\t";} {$3 = $2 + w; print}' | grep -v - | \
+    		fastaFromBed -fi hg38.fa -s -bed stdin -fo ${name:0:-4}_minus_not_scaled.fasta
+
+
+	tn5File=${name:0:-4}_PE2_plus_not_scaled.oneentry.bed
+	awk -v m=$minusshift '{$2 = $2 - m; print}' $tn5File | awk -v w=$winsize '{OFS="\t";} {$3 = $2 + w; print}' | grep -v - | \
+    		fastaFromBed -fi hg38.fa -s -bed stdin -fo ${name:0:-4}_PE2_plus_not_scaled.fasta
+
+	tn5File=${name:0:-4}_PE2_minus_not_scaled.oneentry.bed
+	awk -v m=$minusshift '{$2 = $2 - m; print}' $tn5File | awk -v w=$winsize '{OFS="\t";} {$3 = $2 + w; print}' | grep -v - | \
+    		fastaFromBed -fi hg38.fa -s -bed stdin -fo ${name:0:-4}_PE2_minus_not_scaled.fasta
+
+
+
 done
-
-
-for i in C1_*_PE1_plus.bam
-do
-    name=$(echo $i | awk -F"_PE1_plus.bam" '{print $1}')
-    echo $name
-    seqOutBias hg38.fa ${name}_PE1_plus.bam --no-scale --out=no_scale.tbl \
-                                 --bed=${name}_PE1_plus_no_shift.bed \
-                                 --bw=${name}_PE1_plus_no_shift.bigWig --read-size=30
-    seqOutBias hg38.fa ${name}_PE1_minus.bam --no-scale --out=no_scale.tbl \
-                                 --bed=${name}_PE1_minus_no_shift.bed \
-                                 --bw=${name}_PE1_minus_no_shift.bigWig --read-size=30
-    seqOutBias hg38.fa ${name}_PE2_plus.bam --no-scale --out=no_scale.tbl \
-                                 --bed=${name}_PE2_plus_no_shift.bed \
-                                 --bw=${name}_PE2_plus_no_shift.bigWig --read-size=30
-    seqOutBias hg38.fa ${name}_PE2_minus.bam --no-scale --out=no_scale.tbl \
-                                 --bed=${name}_PE2_minus_no_shift.bed \
-                                 --bw=${name}_PE2_minus_no_shift.bigWig --read-size=30
-    python bedToOneEntryBed.py -i ${name}_PE1_plus_no_shift.bed
-    python bedToOneEntryBed.py -i ${name}_PE1_minus_no_shift.bed
-    python bedToOneEntryBed.py -i ${name}_PE2_plus_no_shift.bed
-    python bedToOneEntryBed.py -i ${name}_PE2_minus_no_shift.bed
-#these bed files can be used to get the sequence flanking ALL Tn5 insertion sites, 
-#so we can see the precise nature of the sequence bias for each PE/strand combination
-    awk '{$2 = $2 - 9; print}' ${name}_PE1_plus_no_shift.oneentry.bed | \
-       awk '{OFS="\t";} {$3 = $2 + 20; print}' | grep -v - | \
-       fastaFromBed -fi hg38.fa -s -bed stdin -fo ${name}_PE1_plus_no_shift.fasta
-    awk '{$2 = $2 - 9; print}' ${name}_PE1_minus_no_shift.oneentry.bed | \
-       awk '{OFS="\t";} {$3 = $2 + 20; print}' |  grep -v - | \
-       fastaFromBed -fi hg38.fa -s -bed stdin -fo ${name}_PE1_minus_no_shift.fasta
-    awk '{$2 = $2 - 9; print}' ${name}_PE2_plus_no_shift.oneentry.bed | \
-       awk '{OFS="\t";} {$3 = $2 + 20; print}' | grep -v - | \
-       fastaFromBed -fi hg38.fa -s -bed stdin -fo ${name}_PE2_plus_no_shift.fasta
-    awk '{$2 = $2 - 9; print}' ${name}_PE2_minus_no_shift.oneentry.bed | \
-       awk '{OFS="\t";} {$3 = $2 + 20; print}' |  grep -v - | \
-       fastaFromBed -fi hg38.fa -s -bed stdin -fo ${name}_PE2_minus_no_shift.fasta
-done
-
-
