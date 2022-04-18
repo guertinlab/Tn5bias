4bc6b50ed23dc7598bdb7d819bd1885dae75ebef
JWolpe <66966976+JWolpe@users.noreply.github.com>
2022-04-18 13:05:21 -0400

    Add files via upload

---
+++ Figure3/DNase_PRE_NNNCNN_log2foldchange.R
@@ -0,0 +1,31 @@
+library(ggplot2)
+################################################################################
+################################################################################
+################################################################################
+
+
+load('DNase_PRE_NNNCNN_log_fold_change.Rdata')
+
+Maskvarplot <- function(){
+  ggplot(DNase_PRE_NNNCNN_log_fold_change, aes(x=factor(Factor), y=Difference, fill = Treatment, color = Treatment)) +
+    xlab('') + ylab(expression(log[2]*('Fold Change'))) +
+    geom_violin() +
+    scale_fill_manual(values=c('#808080', '#FF0000', '#0000FF')) +
+    scale_color_manual(values=c('#808080', '#FF0000', '#0000FF')) +
+    #scale_x_discrete(position = "top") +
+    geom_hline(yintercept=0, linetype="dashed", color = "black") +
+    #geom_point(size = 1, shape = 20) +
+    theme_classic() + 
+    theme(axis.text.x = element_text(angle = 45, vjust = 0.75, hjust=0.75, size = 16, face = 'bold', color = 'black'), 
+          axis.title.y=element_text(size=16, color = 'black', face = 'bold'),
+          axis.title.x = element_text(vjust = -2),
+          axis.text.y = element_text(size = 8, color = 'black', face = 'bold'),
+          axis.line = element_line(colour = 'black', size = 1),
+  legend.title = element_text(size=0),
+  legend.text = element_text(size=16, face = 'bold'))
+}
+
+pdf("DNase_PRE_NNNCNN_testset_log2.pdf", width=12, height=7)
+Maskvarplot()
+dev.off()
+
