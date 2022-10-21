cf0eda262131bce6a9d8c988fadb11eefad01b70
JWolpe <66966976+JWolpe@users.noreply.github.com>
2022-10-21 12:20:39 -0400

    Delete Figure4 directory

--- Figure4/Figure4C_Tn5_Baseline_GCcompare_unscaled_composites.R
+++
@@ -1,7 +0,0 @@
-load('Figure4C_Tn5_GCcon_baseline.Rdata')
-
-pdf("Figure4C_Tn5_unscaled_BaselineVsGCcon.pdf", width=6, height=6)
-plot(V3 ~ V1, data = Tn5_GCcon_baseline, cex = 0, xlab="Baseline Signal", ylab = "Motif GC%", main = "Tn5 baseline vs. motif GC content",
-     cex.lab=1.45, cex.axis=1.45)
-text(V3 ~ V1, labels=V4,data=Tn5_GCcon_baseline, cex=0.75, font=2)
-dev.off()
