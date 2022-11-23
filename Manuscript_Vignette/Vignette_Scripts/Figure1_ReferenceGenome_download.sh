8f4939459b393e867f0c316a3d6932a02db4a7b5
JWolpe <66966976+JWolpe@users.noreply.github.com>
2022-11-23 10:37:19 -0500

    Add files via upload

---
+++ Manuscript_Vignette/Vignette_Scripts/Figure1_ReferenceGenome_download.sh
@@ -0,0 +1,8 @@
+#You will need to download the human and mouse genomes:
+wget http://hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz
+wget https://hgdownload.cse.ucsc.edu/goldenpath/hg38/chromosomes/chrM.fa.gz
+wget http://hgdownload.cse.ucsc.edu/goldenPath/mm39/bigZips/mm39.fa.gz
+#unzip
+gunzip hg38.fa.gz
+gunzip chrM.fa.gz
+gunzip mm39.fa.gz
