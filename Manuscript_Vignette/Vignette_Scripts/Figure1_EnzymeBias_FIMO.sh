c25e379c82d9b7ec29b9fb3ac57283fcef14468a
JWolpe <66966976+JWolpe@users.noreply.github.com>
2022-11-25 10:48:34 -0500

    Update Figure1_EnzymeBias_FIMO.sh

--- Manuscript_Vignette/Vignette_Scripts/Figure1_EnzymeBias_FIMO.sh
+++ Manuscript_Vignette/Vignette_Scripts/Figure1_EnzymeBias_FIMO.sh
@@ -5,7 +5,7 @@ fimo --thresh 0.0001 --text DNase_sepcat_bias.meme \
                             hg38.fa > DNase_sepcat_bias_transfac_0001_fimo.txt
 
 fimo --thresh 0.00009 --text Cyanase_sepcat_bias.meme \
-                             mm39.fa > Cyanase_sepcat_bias_transfac_00007_fimo.txt
+                             mm39.fa > Cyanase_sepcat_bias_transfac_00009_fimo.txt
 
 fimo --thresh 0.00005 --text Benzonase_sepcat_bias.meme \
                              mm39.fa > Benzonase_sepcat_bias_transfac_00005_fimo.txt
