c6ae92eb9f599ec186905187890f38a784be4ace
JWolpe <66966976+JWolpe@users.noreply.github.com>
2022-11-26 13:57:47 -0500

    Update Figure2_seqOutBias_dump.sh

--- Manuscript_Vignette/Vignette_Scripts/Figure2_seqOutBias_dump.sh
+++ Manuscript_Vignette/Vignette_Scripts/Figure2_seqOutBias_dump.sh
@@ -1,5 +1,5 @@
 #This will make a .tbl file of all 3-mers in the genome
-seqOutBias seqtable ../hg38.fa --kmer-size=3 --plus-offset=3 --minus-offset=3 --read-size=76 --out=hg38.3.3.3.tbl
+seqOutBias seqtable ../Figure1/hg38.fa --kmer-size=3 --plus-offset=3 --minus-offset=3 --read-size=76 --out=hg38.3.3.3.tbl
 #This will dump all indexed 3-mer locations to a text file. This file will be LARGE
 seqOutBias dump hg38.3.3.3.tbl > hg38.3.3.3.dump.txt
 #This will take the plus and minus index for CAG and output a bed file for each
@@ -10,14 +10,14 @@ cat hg38.3.3.3plus.19.bed hg38.3.3.3minus.31.bed > CAG_locations.bed
 shuf -n 400000 CAG_locations.bed > CAG_locations_rand400k.bed
 
 #Run seqOutBias with masks designed to correct each peak (mask is shifted 4 base pairs):
-seqOutBias hg38.fa C1_gDNA_rep1.bam --custom-shift=4,-4 --strand-specific --kmer-mask=NNCN \
+seqOutBias ../Figure1/hg38.fa ../Figure1/C1_gDNA_rep1.bam --custom-shift=4,-4 --strand-specific --kmer-mask=NNCN \
                                          --bed=C1_gDNA_rep1_NNNXXXC.bed \
                                          --bw=C1_gDNA_rep1_NNNXXXC.bigWig --read-size=76
 
-seqOutBias hg38.fa C1_gDNA_rep1.bam --custom-shift=4,-4 --strand-specific --kmer-mask=CXXXNNN \
+seqOutBias ../Figure1/hg38.fa ../Figure1/C1_gDNA_rep1.bam --custom-shift=4,-4 --strand-specific --kmer-mask=CXXXNNN \
                                           --bed=C1_gDNA_rep1_NCNN.bed \
                                           --bw=C1_gDNA_rep1_NCNN.bigWig --read-size=76
 
-seqOutBias hg38.fa C1_gDNA_rep1.bam --custom-shift=4,-4 --strand-specific --kmer-mask=CXXXXXXXXNNN \
+seqOutBias ../Figure1/hg38.fa ../Figure1/C1_gDNA_rep1.bam --custom-shift=4,-4 --strand-specific --kmer-mask=CXXXXXXXXNNN \
                                          --bed=C1_gDNA_rep1_CXXXXNNN.bed \
                                          --bw=C1_gDNA_rep1_CXXXXNNN.bigWig --read-size=76
