7077b051aedac0f2b1a768405e437d45012f1ba0
JWolpe <66966976+JWolpe@users.noreply.github.com>
2022-11-27 12:45:40 -0500

    Update Tn5_Bias_Functions.R

--- Manuscript_Vignette/Vignette_Scripts/Tn5_Bias_Functions.R
+++ Manuscript_Vignette/Vignette_Scripts/Tn5_Bias_Functions.R
@@ -442,3 +442,17 @@ uppercase <- function(tableinput){
     else return(v)}))
 }
 
+#Plots seqlogos of JASPAR PSWMs
+plot.seqlogo.meme <-function(infile, outfile = "memefunc_test.pdf") {
+  wd <- getwd()
+  memefile <- paste0(wd,"/",infile)
+  minmeme <- read.csv(memefile, skip = 11, header = FALSE, sep = '')
+  minmeme <- minmeme[1:nrow(minmeme)-1,]
+  meme.trans <- t(minmeme)
+  class(meme.trans) <- "numeric"
+  rownames(meme.trans) = c('A', 'C', 'G', 'T')
+  w =  0.663 + (ncol(meme.trans) + 1)*0.018 + (ncol(meme.trans)+2)* .336
+  pdf(outfile, useDingbats=FALSE, width=w, height=2.695)
+  print(ggseqlogo(meme.trans,  facet = "wrap", font = 'helvetica_bold'))
+  dev.off()
+}
