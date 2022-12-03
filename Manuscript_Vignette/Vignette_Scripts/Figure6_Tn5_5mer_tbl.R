c9768ff79c8eb870e941d98c5f64642c1629ff68
JWolpe <66966976+JWolpe@users.noreply.github.com>
2022-12-03 15:30:14 -0500

    Add files via upload

---
+++ Manuscript_Vignette/Vignette_Scripts/Figure6_Tn5_5mer_tbl.R
@@ -0,0 +1,41 @@
+library(parallel)
+library(bigWig)
+source("https://raw.githubusercontent.com/guertinlab/seqOutBias/master/docs/R/seqOutBias_hcsearch.R")
+#This is the span of bp the 5bp mask will be iterated over
+start_mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+#A function which iterates across the start_mask, creating all possible 5-mers
+neighbors <- function(vecmask) {
+  # generate list of neighboring masks that differ
+  # by the addition of a single unmasked position
+  result <- vector(mode="list", length=length(vecmask))
+  n <- 0
+  for (k in 1:length(vecmask)) {
+    if (vecmask[k] == -1) { # X
+      n <- n + 1
+      vi = vecmask
+      vi[k] = 1 # N
+      vi[k+1] = 1 # 2nd N
+      vi[k+2] = 1 # 3rd N
+      vi[k+3] = 1 # 4th N
+      vi[k+4] = 1 # 5th N
+      result[[n]] <- vec.to.mask(vi)
+    }
+  }
+  if (n == 0) return(NULL)
+  #Remove last line so you don't get an extra N space (mappable bases = mer-1)
+  zz <- (n-4)
+  result[1:zz]
+}
+
+#Create a list of all possible 5-mer positions
+nextlst = neighbors(mask.to.vec(start_mask))
+#seqOutBias arguments passed to each run
+seqOutBias.args = "hg38.fa C1_gDNA_rep1.bam --read-size=76 --strand-specific --custom-shift=4,-4"
+#Command to call seqOutBias
+sqcmd = "seqOutBias"
+#Prefix added to each output file
+prefix = "Tn5_"
+#Apply run.cutmask to each possible 5-mer 
+scores = mclapply(nextlst, function(cutmask) {
+  bw.paths = run.cutmask(cutmask, seqOutBias.args, sqcmd=sqcmd, prefix=prefix, cleanup = FALSE)
+}, mc.cores = 20)
