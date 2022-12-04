e3d63dc4b54d52ffd88cb99b3a7a91aa18c064a3
JWolpe <66966976+JWolpe@users.noreply.github.com>
2022-12-04 14:12:13 -0500

    Update Figure6_Tn5_7mer_frequency.R

--- Manuscript_Vignette/Vignette_Scripts/Figure6_Tn5_7mer_frequency.R
+++ Manuscript_Vignette/Vignette_Scripts/Figure6_Tn5_7mer_frequency.R
@@ -7,7 +7,7 @@ load('../Figure5/hg38_Plus_motif_sequences.Rdata')
 TFseq_plus = lapply(TFseq_plus, mer.positions, mermask = "NNNNNNN")
 #Find frequency of each possible 7-mer for each position
 TFseq_plus = lapply(TFseq_plus, position.frequencies, mermask = "NNNNNNN")
-save(TFseq_plus, file = 'Tn5_TF_position_frequencies_6mer_plus.Rdata')
+save(TFseq_plus, file = 'Tn5_TF_position_frequencies_7mer_plus.Rdata')
 
 #Determine minus strand 7-mer frequencies
 load('../Figure5/hg38_Minus_motif_sequences.Rdata')
@@ -15,4 +15,4 @@ load('../Figure5/hg38_Minus_motif_sequences.Rdata')
 TFseq_minus = lapply(TFseq_minus, mer.positions, mermask = "NNNNNNN")
 #Find frequency of each possible 7-mer for each position
 TFseq_minus = lapply(TFseq_minus, position.frequencies, mermask = "NNNNNNN")
-save(TFseq_minus, file = 'Tn5_TF_position_frequencies_6mer_minus.Rdata')
+save(TFseq_minus, file = 'Tn5_TF_position_frequencies_7mer_minus.Rdata')
