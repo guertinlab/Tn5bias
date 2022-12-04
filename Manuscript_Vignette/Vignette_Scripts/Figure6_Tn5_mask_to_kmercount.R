e57f80340ba414596a27c6e1ee2a6d8a1ce34e94
JWolpe <66966976+JWolpe@users.noreply.github.com>
2022-12-04 15:16:22 -0500

    Update Figure6_Tn5_mask_to_kmercount.R

--- Manuscript_Vignette/Vignette_Scripts/Figure6_Tn5_mask_to_kmercount.R
+++ Manuscript_Vignette/Vignette_Scripts/Figure6_Tn5_mask_to_kmercount.R
@@ -5,4 +5,4 @@ masks = masks[grep('.tbl', masks)]
 registerDoParallel(1)
 foreach (i = 1:length(masks)) %dopar% {system(paste('seqOutBias table ',
         masks[i], ' ../C1_gDNA_rep1.bam > ', substr(masks[i], 1, nchar(masks[i])-4),
-        '_fulldat_scale_factors.txt', sep = ''))}
+        '_scale_factors.txt', sep = ''))}
