6f8e44ae977edd1cd78d5c2b6c01eebd0bcb59de
JWolpe <66966976+JWolpe@users.noreply.github.com>
2022-12-13 12:41:40 -0500

    Delete section2_3.sh

--- Manuscript_Vignette/section2_3.sh
+++
@@ -1,5 +0,0 @@
-
-fimo --thresh 0.00005 --text ATAC_bias_pe1_minus.txt hg38.fa > ATAC_bias_pe1_minus_00005_fimo.txt
-fimo --thresh 0.00005 --text ATAC_bias_pe1_plus.txt hg38.fa > ATAC_bias_pe1_plus_00005_fimo.txt
-fimo --thresh 0.00005 --text ATAC_bias_pe2_minus.txt hg38.fa > ATAC_bias_pe2_minus_00005_fimo.txt
-fimo --thresh 0.00005 --text ATAC_bias_pe2_plus.txt hg38.fa > ATAC_bias_pe2_plus_00005_fimo.txt
