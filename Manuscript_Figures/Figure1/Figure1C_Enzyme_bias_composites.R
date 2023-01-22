9f14f0f7ed65791bf6393e23d39ca9e03148868f
JWolpe <66966976+JWolpe@users.noreply.github.com>
2023-01-22 13:46:35 -0500

    Update Figure1C_Enzyme_bias_composites.R

--- Manuscript_Figures/Figure1/Figure1C_Enzyme_bias_composites.R
+++ Manuscript_Figures/Figure1/Figure1C_Enzyme_bias_composites.R
@@ -1,82 +1,17 @@
-library(bigWig)
-library(zoo)
-library(lattice)
-library(data.table)
-library(matrixStats)
-library(gridExtra)
+source('https://raw.githubusercontent.com/guertinlab/Tn5bias/master/Manuscript_Vignette/Vignette_Scripts/Tn5_Bias_Functions.R')
 ################################################################################
-################################################################################
-################################################################################
-################################################################################
-
-setwd('../output')
-################################################################################
-##Plot.composites takes the composite.lattice object and creates a plot for 
-#this data, while also allowing a mapping of the original motif width onto the
-#plot by specifying a TFlen value (the default is 10). 
-plot.composites <- function(dat, ylabel = '', pdf_name = 'PLEASE_SET_FILE_NAME',
-                            xlabel = '', striplabel = TRUE, legend = TRUE,
-                            motifline = FALSE, Motiflen = 10,
-                            figwidth = 2.5, figheight=3,
-                            indexlist = NULL, layoutgrid = NULL,
-                            col.lines = c("#0000FF", "#FF0000",  
-                                          rgb(0.1,0.5,0.05,1/2), rgb(0,0,0,1/2),  
-                                          rgb(1/2,0,1/2,1/2), rgb(0,1/2,1/2,1/2), 
-                                          rgb(1/2,1/2,0,1/2)), 
-                            fill.poly = c(rgb(0,0,1,1/4), 
-                                          rgb(1,0,0,1/4), rgb(0.1,0.5,0.05,1/4), 
-                                          rgb(0,0,0,1/4), rgb(1/2,0,1/2,1/4))) {
-  require(lattice)
-  pdf(paste(pdf_name, '.pdf', sep = ''), width= figwidth, height= figheight) 
-  print(xyplot(est ~ x|factor, group = group, data = dat, strip = striplabel,
-               type = 'l', as.table = TRUE,
-               scales=list(x=list(at=seq(-15,15,5), cex=1.1, relation = "free", axs ="i", rot = 45), 
-                           y =list(cex=1.1, relation="free", tick.number=4)),
-               col = col.lines,
-               auto.key = if (legend == TRUE) 
-               {list(points=F, lines=T, cex=0.8)} else{},
-               par.settings = list(strip.background=list(col="#00000000"),
-                                   strip.border = list(col = 'transparent'),
-                                   superpose.symbol = list(pch = c(16),
-                                                           col=col.lines, 
-                                                           cex =0.5), 
-                                   superpose.line = list(col = col.lines, 
-                                                         lwd=c(2), 
-                                                         lty = c(1))),
-               cex.axis=1.0,
-               par.strip.text=list(cex=1.2, font=1, col='black'),
-               aspect=1.0,
-               between=list(y=0.5, x=0.5),
-               lwd=2,
-               ylab = list(label = paste(ylabel), cex =1.3),
-               xlab = list(label = paste(xlabel), cex =1.3),
-               xlim = c(-15,15),
-               index.cond = indexlist,
-               layout = layoutgrid,
-               panel = function(x, y, ...) {
-                 panel.xyplot(x, y, ...)
-                 #panel.abline(h = 0, lty =1, lwd = 1.0, col = '#A9A9A932')
-                 level = dimnames(trellis.last.object())[["factor"]][packet.number()]
-                 if (motifline == TRUE) 
-                 {panel.abline(v = Motiflen[rownames(Motiflen)==level,]/2, lty = 2, col = "red")} else{}
-                 if (motifline == TRUE) 
-                 {panel.abline(v = -Motiflen[rownames(Motiflen)==level,]/2, lty = 2, col = "red")} else{}
-               }
-  ))
-  dev.off()
-}
-
-
 load('../data/Figure1C_Unscaled_enzyme_composites.Rdata')
-ssplot <- plot.composites(unscaled_composite_agg, legend = FALSE, 
-                          ylabel = 'Cut Frequency',
-                          xlabel = 'Distance from Motif Center',
-                          figwidth = 4, figheight = 12,
-                          motifline = FALSE,
-                          Motiflen = mlen,
-                          striplabel = TRUE,
-                          pdf_name = 'unscaled_striplabel_ss_composites',
-                          indexlist = list(c(5,3,2,1,4)),
-                          layoutgrid = c(1,5)
+###Plot these all together
+enzymebiasplot = plot.composites(unscaled_composite_agg, x_axis_range=-15:15,
+                                 legend = FALSE, 
+                                 ylabel = 'Cut Frequency',
+                                 xlabel = 'Distance from Motif Center',
+                                 figwidth = 4, figheight = 12,
+                                 motifline = FALSE,
+                                 Motiflen = mlen,
+                                 striplabel = TRUE,
+                                 pdf_name = 'unscaled_enzymebias_composites',
+                                 indexlist = list(c(5,3,2,1,4)),
+                                 layoutgrid = c(1,5),
+                                 y_axis_range = 0:1, y_axis = TRUE, Y_axis_ticks = seq(0,1,0.5),
 )
-
